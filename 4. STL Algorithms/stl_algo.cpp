40ce9440986591784a16246bb1d36e82c70c1d76
DufanGratis <evan.hartoni@gmail.com>
2022-07-16 13:15:47 +0700

    Day 6

--- 4. STL Algorithms/stl_algo.cpp
+++ 4. STL Algorithms/stl_algo.cpp
@@ -9,7 +9,6 @@ int main()
 {
     array<int, 5> x{6, 7, 8, 9, 1};
     array<int, 5> y{1, 2, 3, 4, 5};
-    array<int, 5> z{1, 2, 3, 2, 1};
 
     cout << "x = ";
     for (auto elem : x)
@@ -51,34 +50,89 @@ int main()
     else
         cout << "Array x does not contains the element " << n << endl;
 
+    array<int, 5> z{1, 2, 3, 2, 1};
+
     cout << "z = ";
     for (auto elem : z)
         cout << elem << " x ";
     cout << endl;
     auto eq = equal(z.begin(), z.begin() + z.size() / 2, z.rbegin());
-    cout << "Array z is " << ((eq) ? "" : "not ") << "a palindrome" << endl;
+    cout << "Array z is" << ((eq) ? " " : " not ") << "a palindrome" << endl;
 
-    // mismatch
+    string m1 = "abcdba";
+    string m2 = "abcdea";
+    auto mm = mismatch(m1.begin(), m1.end(), m2.begin());
+    cout << "The same elements of " << m1 << " and " << m2 << " are ";
+    for (auto m = m1.begin(); m != mm.first; ++m)
+        cout << *m;
+    cout << " and stops at " << *mm.first << " on abcdba and " << *mm.second << " on abcdea" << endl;
 
-    // search
+    string n1 = "abcdba";
+    string n2 = "abce";
+    auto ns = search(n1.begin(), n1.end(), n2.begin(), n2.end());
+    cout << n1 << ((ns != n1.end()) ? " does" : " does not") << " contains the string " << n2 << endl;
 
     // Modifying
 
-    // copy
+    vector<int> p{1, 2, 3, 2, 2, 1};
+    vector<int> q;
 
-    // unique
+    copy(p.begin(), p.end(), back_inserter(q));
+    cout << "p = ";
+    for (auto elem : p)
+        cout << elem << " - ";
+    cout << endl;
 
-    // remove
+    cout << "q = ";
+    for (auto elem : q)
+        cout << elem << " - ";
+    cout << endl;
 
-    // replace
+    auto un = unique(q.begin(), q.end());
+    q.erase(un, q.end());
 
-    // rotate
+    cout << "q unique = ";
+    for (auto q_itr = q.begin(); q_itr != q.end(); ++q_itr)
+        cout << *q_itr << " - ";
+    cout << endl;
 
-    // random_shuffle
+    remove(q.begin(), q.end(), 3);
+    replace(q.begin(), q.end(), 1, -1);
 
-    // partition
+    cout << "q replace = ";
+    for (auto q_itr = q.begin(); q_itr != q.end(); ++q_itr)
+        cout << *q_itr << " - ";
+    cout << endl;
 
-    // Permutation
+    sort(p.begin(), p.end());
+    cout << "p sort = ";
+    for (auto elem : p)
+        cout << elem << " - ";
+    cout << endl;
+
+    rotate(p.begin(), p.begin() + 1, p.end());
+    cout << "p rotate left = ";
+    for (auto elem : p)
+        cout << elem << " - ";
+    cout << endl;
+
+    random_shuffle(p.begin(), p.end());
+    cout << "p random = ";
+    for (auto elem : p)
+        cout << elem << " - ";
+    cout << endl;
+
+    auto even = partition(p.begin(), p.end(), [](int pp)
+                          { return pp % 2 == 0; });
+    cout << "p even = ";
+    for (auto p_even = p.begin(); p_even != even; ++p_even)
+        cout << *p_even << " - ";
+    cout << endl;
+
+    cout << "p odd = ";
+    for (auto p_odd = even; p_odd != p.end(); ++p_odd)
+        cout << *p_odd << " - ";
+    cout << endl;
 
     z.fill(-1);
 
@@ -188,6 +242,8 @@ int main()
         cout << "kangaroa is smaller than kangaroo" << endl;
 
     // Min and Max
+
+    cout << "The max element of x is " << *max_element(x.begin(), x.end()) << " and the min element of x is " << *min_element(x.begin(), x.end()) << endl;
 
     x.~array();
     y.~array();
